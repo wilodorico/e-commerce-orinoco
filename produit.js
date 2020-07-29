6dbafd8eb024d3a4ef04d392a3c1b107af0d5660
wilfried <wilfried.odorico@gmail.com>
2020-07-29 18:34:46 +0200

    ajout de la page produit + passage des ref api en singleproduct

--- produit.js
+++ produit.js
@@ -1,15 +1,12 @@
-// Requête API
 let request = new XMLHttpRequest();
 request.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        let response = JSON.parse(this.responseText);
+        let item = JSON.parse(this.responseText);
         const main = document.getElementById('main');
         //console.log(response);
-        
-// Boucle récupération des données produits
-        for (const item of response) {
+        //console.log(item.name);
             let prix = item.price / 100;
-            //console.log(item.name);
+            
             let card = 
             `<div class="col-lg-4 col-md-6">
                 <div class="card m-3 shadow border-0">
@@ -18,20 +15,33 @@ request.onreadystatechange = function() {
                     <h5 class="card-title">${item.name}</h5>
                 <div class="d-flex justify-content-between align-items-center">
                   <div class="btn-group">
-                    <a href="produit.html?id=${item._id}" type="button" class="btn btn-sm btn-secondary">Voir le produit</a>
+                    <button type="button" class="btn btn-sm btn-secondary">Voir le produit</button>
                   </div>
                   <small class="text-muted">${prix.toFixed(2)} €</small>
+                  <select id="vernis"></select>
                 </div>
               </div>
             </div>
           </div>`;
             main.innerHTML += card;
+
+            const vernis = document.getElementById('vernis');
+            for(let choice of item.varnish){
+              vernis.innerHTML += '<option value="'+ choice +'">'+ choice +'</option>';
+            }
             //console.log(item);
-        }
+        
     }
 };
-request.open("GET", "http://localhost:3000/api/furniture");
+const urlParam = new URLSearchParams(window.location.search);
+//console.log(urlParam.get('id'));
+
+
+request.open("GET", "http://localhost:3000/api/furniture/" + urlParam.get('id'));
 request.send();
+
+//document.getElementById('vernis').value; recupérer le vernis selectionné pour panier
+
 
 
 
